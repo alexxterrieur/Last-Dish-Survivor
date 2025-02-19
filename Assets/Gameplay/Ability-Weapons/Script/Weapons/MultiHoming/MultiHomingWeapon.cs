1521c124beb3c09f03906f24a28a439ec3249452
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2025-02-19 17:17:59 +0100

    multi homing weapon + fix

---
+++ Assets/Gameplay/Ability-Weapons/Script/Weapons/MultiHoming/MultiHomingWeapon.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+[CreateAssetMenu(fileName = "MultiHomingWeapon", menuName = "Scriptable Objects/Weapon/MultiHomingWeapon")]
+public class MultiHomingWeapon : Weapon
+{
+    public float projectileSpeed;
+    public int maxTargets;
+
+    public override void Activate(GameObject user)
+    {
+        base.Activate(user);
+
+        List<Transform> targets = FindClosestEnemies(user, maxTargets);
+        if (targets.Count == 0)
+        {
+            Debug.Log("No enemies found");
+            return;
+        }
+
+        foreach (Transform target in targets)
+        {
+            GameObject instance = Instantiate(abilityPrefab, user.transform.position, Quaternion.identity);
+            HomingProjectile homingProjectile = instance.GetComponent<HomingProjectile>();
+
+            if (homingProjectile != null)
+            {
+                homingProjectile.SetTarget(target, projectileSpeed);
+                homingProjectile.damage = damage;
+            }
+        }
+    }
+
+    private List<Transform> FindClosestEnemies(GameObject user, int maxCount)
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        return enemies.OrderBy(enemy => Vector3.Distance(user.transform.position, enemy.transform.position)).Take(maxCount).Select(enemy => enemy.transform).ToList();
+    }
+}
